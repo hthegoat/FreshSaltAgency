1474ccf662cbdcd99f616e57668bdbac92354400
Harrison <56010813+hthegoat@users.noreply.github.com>
2022-07-06 23:54:02 -0400

    fb

--- plugins/fb.js
+++ plugins/fb.js
@@ -2,4 +2,19 @@ import facebook from 'nuxt-facebook-pixel-module'
 
 facebook()
 
-fbq('init')
+fbq('init',
+{
+  em: 'email@email.com',         //Values will be hashed automatically by the pixel using SHA-256
+  fn: 'first_name',
+  ln: 'last_name',
+  ph: 'phone_number',
+  ct: 'city',
+  st: 'state',
+  zp: 'zip_code',
+  country: 'country',
+  external_id: 'external_id',
+
+
+
+}
+)
